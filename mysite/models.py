27b5f31d0a7eaac687f8a2e451b17c314dbf7291
chenx <bufubaoni@163.com>
2016-11-01 23:03:46 +0800

    workflow

--- mysite/models.py
+++ mysite/models.py
@@ -1,5 +1,18 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
+from river.models.fields.state import StateField
+from river.models.managers.wofkflow_object import WorkflowObjectManager
+
 
 # Create your models here.
+
+
+class MyModel(models.Model):
+    testflow = models.CharField(max_length=50, default="6666", verbose_name="测试")
+    state = StateField()
+    mnage = WorkflowObjectManager()
+
+    def __unicode__(self):
+        return self.testflow
