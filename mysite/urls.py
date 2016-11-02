47c3813e24f4bda10bb2df55bfc0ccee0a64b995
chenx <bufubaoni@163.com>
2016-11-02 21:46:41 +0800

    login

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
 import views
+
 urlpatterns = [
     url(r'^$', views.test),
+    url(r'^login/$', views.loginme, name="login")
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,15 +1,35 @@
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django import forms
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from requests import sessions
 from river.models import State
 
 from mysite.models import MyModel
+import pdb
 
 
 def test(request):
-    my = MyModel.objects.get(pk=2)
-    user = User.objects.get(username=request.user)
-    my.proceed(user)
+    # my = MyModel.objects.get(pk=2)
+    # user = User.objects.get(username=request.user)
+    # my.proceed(user)
     # my.proceed(user, next_state=State.objects.get(label='start'))
-    return render(request, "test.html", {"test1": my})
+    return render(request, "test.html", {"test1": request.user})
+
+
+def loginme(request):
+    class loginform(forms.Form):
+        username = forms.CharField(label='username')
+        password = forms.CharField(label='password', widget=forms.PasswordInput)
+
+    if request.method == "POST":
+        form = loginform(request.POST)
+        user = authenticate(username=form.data['username'],
+                            password=form.data['password'])
+        if user is not None:
+            login(request, user)
+            return redirect("/mysite")
+
+    return render(request, "login.html", {"form": loginform()})
