9b928fe1c24a682e933174eb7c65c10b2a48f09b
chenx <bufubaoni@163.com>
2016-11-02 20:33:47 +0800

    work flow

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
+from river.models import State
+
+from mysite.models import MyModel
+
+
+def test(request):
+    my = MyModel.objects.get(pk=2)
+    user = User.objects.get(username=request.user)
+    my.proceed(user)
+    # my.proceed(user, next_state=State.objects.get(label='start'))
+    return render(request, "test.html", {"test1": my})
