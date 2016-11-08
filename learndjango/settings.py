1ec11d8009fd60c27790f450cc08b81a75ee32cf
chenx <bufubaoni@163.com>
2016-11-08 22:32:07 +0800

    use mptt

--- learndjango/settings.py
+++ learndjango/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'mysite.apps.MysiteConfig',
     'django.conf.urls',
     'river',
+    'mptt'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'learndjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['c:/Python27/Lib/site-packages/mptt/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
