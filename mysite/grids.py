5018a8ce3fca377853e57152b433446dbdb626ea
chenx <bufubaoni@163.com>
2016-11-14 22:05:08 +0800

    add tables

---
+++ mysite/grids.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from models import MyModel
+import django_tables2 as tables
+
+
+class MyModelTable(tables.Table):
+    # attrs = {"class": "table  table-bordered table-hover"}
+    class Meta:
+        model = MyModel
+        attrs = {'class': 'table  table-bordered table-hover'}
+
