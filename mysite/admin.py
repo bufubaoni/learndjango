c7dfa1e9f278f335ac978c9859ecd1f28a542f62
chenx <bufubaoni@163.com>
2016-11-19 22:38:35 +0800

    custom admin for mymodel

--- mysite/admin.py
+++ mysite/admin.py
@@ -2,13 +2,13 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import MyModel, somework, MenuItem, CustomUser,MenuGroup
+from .models import MyModel, somework, MenuItem, CustomUser, MenuGroup
 from mptt.admin import DraggableMPTTAdmin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdim
 from django.contrib.auth.models import User
 
 # Register your models here.
-admin.site.register(MyModel)
+
 admin.site.register(somework)
 admin.site.register(MenuGroup)
 admin.site.register(MenuItem, DraggableMPTTAdmin)
@@ -20,11 +20,15 @@ class Custom(admin.StackedInline):
     verbose_name_plural = "扩展属性"
 
 
-
 class UserAdmin(BaseUserAdim):
+    inlines = (Custom,)
 
-    inlines = (Custom, )
 
+class MyModelAdmin(admin.ModelAdmin):
+    list_display = ('testflow', 'state')
+
+
+admin.site.register(MyModel, MyModelAdmin)
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
