1720bcb38a5b0abc7d5cae283dcf3dc6166b168b
chenx <bufubaoni@163.com>
2016-11-25 14:27:44 +0800

    add some auth

--- mysite/serial.py
+++ mysite/serial.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 # Created by Alex on 2016/11/25
 from rest_framework import serializers
+from river.utils.exceptions import RiverException
+
 from models import MyModel
 from river.models import State
 import json
@@ -9,16 +11,25 @@ import json
 
 class MyModelSerializer(serializers.HyperlinkedModelSerializer):
     state_display = serializers.SerializerMethodField()
-    state = serializers.PrimaryKeyRelatedField(
-        queryset=State.objects.all()
-    )
+
+    # state = serializers.PrimaryKeyRelatedField(
+    #     queryset=State.objects.all()
+    # )
 
     class Meta:
         model = MyModel
-        fields = ('id', 'testflow', 'state', 'state_display')
-
+        fields = ('id', 'testflow', 'state_id', 'state_display')
 
     def get_state_display(self, obj):
         # import pdb
         # pdb.set_trace()
         return obj.state.label
+
+    def create(self, validated_data):
+        mymodel = MyModel.objects.create(**validated_data)
+        try:
+            mymodel.proceed(self.context['request'].user)
+            mymodel.save()
+        except RiverException as e:
+            mymodel.delete()
+        return mymodel
